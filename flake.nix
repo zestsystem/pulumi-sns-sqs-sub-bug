8332b75f6a5719751cae22be87c85b31bc09d600
zestsystem <mikeyim@unitetheculture.org>
2024-02-26 19:52:13 -0800

    feat: setup nix flake

---
+++ flake.nix
@@ -0,0 +1,38 @@
+{
+  description = "Test Pulumi SNS and SQS";
+  inputs = {
+    nixpkgs = {
+      url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    };
+    flake-utils = {
+      url = "github:numtide/flake-utils";
+    };
+  };
+
+  outputs = {
+    nixpkgs,
+    flake-utils,
+    ...
+  }:
+    flake-utils.lib.eachDefaultSystem (system: let
+      pkgs = nixpkgs.legacyPackages.${system};
+      corepackEnable = pkgs.runCommand "corepack-enable" {} ''
+        mkdir -p $out/bin
+        ${pkgs.nodejs-18_x}/bin/corepack enable --install-directory $out/bin
+      '';
+    in {
+      formatter = pkgs.alejandra;
+      devShells = {
+        default = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            nodejs-18_x
+            doppler
+            pulumi
+            pulumiPackages.pulumi-language-nodejs
+            awscli
+            corepackEnable
+          ];
+        };
+      };
+    });
+}
