07e9db00cc61f73483113fc1577e7babf3a966a8
zestsystem <mikeyim@unitetheculture.org>
2024-02-26 21:40:41 -0800

    feat: setup fastify server

---
+++ server/src/index.js
@@ -0,0 +1,25 @@
+// Require the framework and instantiate it
+
+// ESM
+import Fastify from "fastify";
+const fastify = Fastify({
+    logger: true,
+});
+
+// Declare a route
+fastify.get("/", function(request, reply) {
+    reply.send({ hello: "world" });
+});
+
+fastify.get("/test-queue", async function(request, reply) {
+    return reply.send({ message: "Test queue successful!" });
+});
+
+// Run the server!
+fastify.listen({ port: 3000 }, function(err, address) {
+    if (err) {
+        fastify.log.error(err);
+        process.exit(1);
+    }
+    // Server is now listening on ${address}
+});
